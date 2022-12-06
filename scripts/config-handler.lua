023e8c5bfc1dacce1bc2303f9ba5546165ea248c
Tom Burrows <tomburrows13@icloud.com>
2022-12-06 02:22:53 +0000

    Add handle_empties()

--- scripts/config-handler.lua
+++ scripts/config-handler.lua
@@ -1,3 +1,28 @@
+local function handle_empties()
+  local empty_every = 2  -- TODO: get value from config
+  local tiers_since_last_empty = 1
+  for tier, tier_list in pairs(global.modules_by_tier) do
+    local some_enabled = false
+    for _, module in pairs(tier_list) do
+      if module.type == "empty" then
+        -- We have reached the end of the list of modules of this tier
+        if some_enabled and tiers_since_last_empty >= empty_every then
+          -- At least one module of this tier is enabled, so we can enable the empty module
+          module.enabled = true
+          tiers_since_last_empty = 1
+        else
+          -- All modules of this tier are disabled, so we can disable the empty module
+          module.enabled = false
+          tiers_since_last_empty = tiers_since_last_empty + 1
+        end
+      end
+      if module.enabled then
+        none_enabled = true
+      end
+    end
+  end
+end
+
 script.on_event(defines.events.on_research_finished,
   function(event)
     local technology = event.research
@@ -19,6 +44,7 @@ script.on_event(defines.events.on_research_finished,
                     end
                   end
                 end
+                handle_empties()
               end
             end
           end
